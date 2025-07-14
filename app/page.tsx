fba0fc52100293ac01a24b08749a12b2e65e1858
UeMHRB <muhammadhamzarafiquebhatti4440@gmail.com>
2025-07-14 06:52:01 -0700

    Developing HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,49 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div >
-      <h1 className='text-3xl  underline'>
-        Welcome to the Saas App!
+    <main>
+      <h1 className='text-2xl underline'>
+        Popular Companions
       </h1>
-      <Button>
-        Let's Get Started
-      </Button>
-    </div>
+
+      <section className='home-section' >
+        <CompanionCard
+          id = '123'
+          name = 'Neura the Brainy Explorer'
+          topic = 'Neural Network of the Brain'
+          subject = 'Science'
+          duration = {45}
+          color = '#ffda6e'
+        />
+         <CompanionCard
+          id = '456'
+          name = 'Countsy the Number Wizard'
+          topic = 'Derivatives and Integrals'
+          subject = 'Math'
+          duration = {30}
+          color = '#e5d0ff'
+        />
+         <CompanionCard
+          id = '789'
+          name = 'Verba the Vocabulary Builder'
+          topic = 'English Literature'
+          subject = 'Language'
+          duration = {30}
+          color = '#ffda6e'
+        />
+        
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList/>
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
